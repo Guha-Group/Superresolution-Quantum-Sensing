bf79dc86226b83c96ad76df21eb12a7e1fbe77f1
NicoDeshler <nico.deshler@gmail.com>
2025-04-04 13:11:20 -0700

    messed up last monte-carlo submission. Changed mat files and updated slurm walltime

--- 2-Source/BayesianReceiver_MonteCarlo.m
+++ 2-Source/BayesianReceiver_MonteCarlo.m
@@ -23,13 +23,28 @@ addpath('utils/')
 % setup the monte-carlo survey ranges
 sigma = 1;
 x0 = 0;
-s_range = sigma*(.05:.0125:.5);
 k_range = 0:.1:.4;
+s_range = sigma*linspace(.01,.5,50);
 T = 500; % Trials per monte-carlo sample
-M = 5e4;
-N = 5e4;
+M = 1e4;
+N = 1e4;
 photons_per_adaptation = 1e3;
 splitting_ratio = .5; 
+
+
+% collect all simulation variables and meta-data into a structure
+sim_struct.directory_name = directory_name;
+sim_struct.array_id = array_id;
+sim_struct.sigma = sigma;
+sim_struct.x0 = x0;
+sim_struct.k_range = k_range;
+sim_struct.s_range = s_range;
+sim_struct.T = T;
+sim_struct.M = M;
+sim_struct.N = N;
+sim_struct.photons_per_adaptation = photons_per_adaptation;
+sim_struct.splitting_ratio = splitting_ratio;
+
 
 % containers for holding results
 XSK_0 = zeros(3,numel(s_range),numel(k_range),T);
@@ -41,43 +56,44 @@ PHOTONS_AB = zeros(2,numel(s_range),numel(k_range),T);
 parpool(num_workers)
 
 % run Monte-Carlo Survey
-%for ns = 1:numel(s_range)
-    ns = array_id;
-    s = s_range(array_id);
-    for nk = 1:numel(k_range)
-        k = k_range(nk);
-        parfor t = 1:T
-
-            % display current iter
-            disp([ns,nk,t])
-
-            % random number generation seed should be unique for each
-            % worker in parfor loop
-            rng(ns+nk+t);
-
-            % construct the parameter vector
-            [xsk_0] = [x0,s,k];
-
-            %% Direct Imaging
-            [xsk_DI,~] = SimulateReceiver(xsk_0,N,'DirectImaging',M);
-            
-            %% Static BSPADE
-            [xsk_SB,~] = SimulateReceiver(xsk_0,N,'StaticBSPADE',M,splitting_ratio);
-            
-            %% Adaptive BSPADE
-            [xsk_AB,PDF_AB] = SimulateReceiver(xsk_0,N,'AdaptiveBSPADE',M,photons_per_adaptation);
-            
-            % store results
-            XSK_EST_DI(:,array_id,nk,t) = xsk_DI;
-            XSK_EST_SB(:,ns,nk,t) = xsk_SB;
-            XSK_EST_AB(:,ns,nk,t) = xsk_AB;
-            PHOTONS_AB(:,ns,nk,t) = PDF_AB.photons(1:2);
-
-        end
+nk = array_id;
+k = k_range(nk);
+for ns = 1:numel(s_range)
+    s = s_range(ns);
+    parfor t = 1:T
+    
+        % display current iter
+        disp([nk,ns,t])
+    
+        % random number generation seed should be unique for each
+        % worker in parfor loop
+        rng(nk+ns+t);
+    
+        % construct the parameter vector
+        xsk_0 = [x0,s,k];
+    
+        %% Direct Imaging
+        [xsk_DI,~] = SimulateReceiver(xsk_0,N,'DirectImaging',M);
         
-        % save 
-        save(fullfile(directory_name,['MonteCarlo_2Source_ReceiverSurvey',num2str(array_id),'.mat']),'-regexp', '^(?!ans$).')
+        %% Static BSPADE
+        [xsk_SB,~] = SimulateReceiver(xsk_0,N,'StaticBSPADE',M,splitting_ratio);
+        
+        %% Adaptive BSPADE
+        [xsk_AB,PDF_AB] = SimulateReceiver(xsk_0,N,'AdaptiveBSPADE',M,photons_per_adaptation);
+        
+        % store results
+        XSK_0(:,nk,ns,t) = xsk_0;
+        XSK_EST_DI(:,nk,ns,t) = xsk_DI;
+        XSK_EST_SB(:,nk,ns,t) = xsk_SB;
+        XSK_EST_AB(:,nk,ns,t) = xsk_AB;
+        PHOTONS_AB(:,nk,ns,t) = PDF_AB.photons(1:2);
+    
     end
+    
+    % save 
+    save(fullfile(directory_name,['MonteCarlo_2Source_ReceiverSurvey',num2str(array_id),'.mat']),...
+        'sim_struct','XSK_0','XSK_EST_DI','XSK_EST_SB','XSK_EST_AB','PHOTONS_AB')
+end
 %end
 end
 
